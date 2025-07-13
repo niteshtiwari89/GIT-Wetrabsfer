0dcbbe7a8a2170871ecf73c83e5168c7e68ccc8c
NiteshTiwari89 <tiwarinitesh667@gmail.com>
2025-07-13 11:51:45 +0530

    Updated Api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://git-wetrabsfer.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE,
@@ -52,4 +52,4 @@ export const deleteBooking = async (bookingId) => {
 
 
 // Health Check
-export const healthCheck = () => axios.get('http://localhost:5000/health');
+export const healthCheck = () => axios.get('https://git-wetrabsfer.vercel.app/health');
